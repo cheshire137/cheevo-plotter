100b1ec48a616c5adf85798a1e57e92bdcce2612
Sarah Vessels <cheshire137@gmail.com>
2016-01-02 16:55:26 -0500

    Add Steam interface and CORS for client

---
+++ src/actions/steam.js
@@ -0,0 +1,21 @@
+import fetch from '../core/fetch';
+import Config from '../config.json';
+
+class Steam {
+  static async getSteamId(username) {
+    return this.makeRequest('/api/steam' +
+                            '?path=/ISteamUser/ResolveVanityURL/v0001/' +
+                            '&vanityurl=' + username);
+  }
+
+  static async makeRequest(path) {
+    const url = Config[process.env.NODE_ENV].serverUri + path +
+                (path.indexOf('?') > -1 ? '&' : '?') +
+                'format=json';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+}
+
+export default Steam;
