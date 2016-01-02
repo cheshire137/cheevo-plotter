dd8352430c2a03a967f76f4effdfd99b0e97ce9d
Sarah Vessels <cheshire137@gmail.com>
2016-01-02 13:26:53 -0500

    Remove some unused generated pages

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -23,12 +23,6 @@ class Navigation extends Component {
   render() {
     return (
       <div className={cx(s.root, this.props.className)} role="navigation">
-        <a className={s.link} href="/about" onClick={Link.handleClick}>About</a>
-        <a className={s.link} href="/contact" onClick={Link.handleClick}>Contact</a>
-        <span className={s.spacer}> | </span>
-        <a className={s.link} href="/login" onClick={Link.handleClick}>Log in</a>
-        <span className={s.spacer}>or</span>
-        <a className={cx(s.link, s.highlight)} href="/register" onClick={Link.handleClick}>Sign up</a>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -12,9 +12,7 @@ import Router from 'react-routing/src/Router';
 import fetch from './core/fetch';
 import App from './components/App';
 import ContentPage from './components/ContentPage';
-import ContactPage from './components/ContactPage';
 import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
 import NotFoundPage from './components/NotFoundPage';
 import ErrorPage from './components/ErrorPage';
 
@@ -24,11 +22,7 @@ const router = new Router(on => {
     return component && <App context={state.context}>{component}</App>;
   });
 
-  on('/contact', async () => <ContactPage />);
-
   on('/login', async () => <LoginPage />);
-
-  on('/register', async () => <RegisterPage />);
 
   on('*', async (state) => {
     const response = await fetch(`/api/content?path=${state.path}`);
