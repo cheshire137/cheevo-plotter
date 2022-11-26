df34de487370700d7e4c610365e9523bdc3eb16f
Sarah Vessels <cheshire137@gmail.com>
2022-11-26 11:45:12 -0600

    Restore /api/steam route
    Removed in https://github.com/cheshire137/cheevo-plotter/commit/0f6aa60ae2fb62fb7c404f9dc468410c0870bc53#diff-79f4c7cdc0b026f6e556196f5911a449157807cffdb3f9d33f0b5404b4ffc2f3

--- server.js
+++ server.js
@@ -12,6 +12,33 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/api/steam', async (req, res, next) => {
+  let url = req.query.path;
+  let isXml = false;
+  for (var key in req.query) {
+    if (key !== 'path') {
+      const joiner = url.indexOf('?') > -1 ? '&' : '?';
+      url = url + joiner + key + '=' + encodeURIComponent(req.query[key]);
+    }
+    if (key === 'xml') {
+      isXml = true;
+    }
+  }
+  if (isXml) {
+    url = 'http://steamcommunity.com' + url;
+  } else {
+    url = 'http://api.steampowered.com' + url +
+          (url.indexOf('?') > -1 ? '&' : '?') + 'key=' +
+          process.env.STEAM_API_KEY;
+  }
+  const response = await fetch(url);
+  const data = isXml ? await response.text() : await response.json();
+  if (isXml) {
+    res.set('Content-Type', 'text/xml');
+  }
+  res.send(data);
+});
+
 const port = process.env.PORT || 8080;
 console.log("Starting server on port " + port);
 app.listen(port);
