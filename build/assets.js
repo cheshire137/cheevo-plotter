0f6aa60ae2fb62fb7c404f9dc468410c0870bc53
Sarah Vessels <cheshire137@gmail.com>
2022-11-26 10:43:08 -0600

    New React app, remove React Starter Kit
    npx create-react-app cheevo-plotter-new
    
    then copied over the files. Dropping existing custom styling, will replace with an updated look.

--- build/assets.js
+++
@@ -1,1 +0,0 @@
-module.exports = {"main":{"js":"/main.1182b15b4955ece5406c.js"}};
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-npm run build
-git add build/ && git commit -m "Update build/"
-git subtree push --prefix build heroku master
