f177f8c59ba85473cf654c0aa48005c38b8369c5
Sarah Vessels <cheshire137@gmail.com>
2022-11-26 11:44:51 -0600

    Restore default /ping route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
