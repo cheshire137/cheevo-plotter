448d727ddb122d6c36e64fcfec48646084fdf2af
Sarah Vessels <cheshire137@gmail.com>
2022-11-26 11:18:41 -0600

    Use TypeScript for local storage model

--- src/stores/LocalStorage.ts
+++ src/stores/LocalStorage.ts
@@ -1,4 +1,4 @@
-import Config from '../config.json';
+const localStorageKey = "cheevo-plotter";
 
 class LocalStorage {
   static getJSON() {
@@ -9,31 +9,28 @@ class LocalStorage {
       console.error('browser does not support local storage');
       return {};
     }
-    var appData =
-        window.localStorage.getItem(Config[process.env.NODE_ENV].localStorageKey) || '{}';
+    var appData = window.localStorage.getItem(localStorageKey) || '{}';
     return JSON.parse(appData);
   }
 
-  static get(key) {
+  static get(key: string) {
     var appData = this.getJSON();
     return appData[key];
   }
 
-  static set(key, value) {
+  static set(key: string, value: any) {
     var appData = this.getJSON();
     appData[key] = value;
     if (typeof window !== 'undefined') {
-      window.localStorage.setItem(Config[process.env.NODE_ENV].localStorageKey,
-                                  JSON.stringify(appData));
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData));
     }
   }
 
-  static delete(key) {
+  static delete(key: string) {
     var appData = this.getJSON();
     delete appData[key];
     if (typeof window !== 'undefined') {
-      window.localStorage.setItem(Config[process.env.NODE_ENV].localStorageKey,
-                                  JSON.stringify(appData));
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData));
     }
   }
 }
