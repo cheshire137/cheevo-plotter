9fdeb56ba31555cbb3e1743c901743592b54ccb9
Sarah Vessels <cheshire137@gmail.com>
2022-11-27 16:20:01 -0600

    Update AchievementComparison

---
+++ src/components/AchievementComparison.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+interface Props {
+  players: any[];
+  achievement: any;
+}
+
+const AchievementComparison = ({ players, achievement }: Props) => {
+  const playerIds: string[] = Object.keys(achievement.players);
+
+  return <li>
+    {typeof achievement.iconUri === 'string' ? <img src={achievement.iconUri} alt={achievement.name}
+      width="64" height="64" /> : null}
+    <div>
+      <h2>{achievement.name}</h2>
+      <ul>
+        {playerIds.map(playerId => <li key={playerId}>{players[playerId].personaname}</li>)}
+      </ul>
+    </div>
+  </li>
+}
+
+export default AchievementComparison;
