8cd681eaaee3739ce3f1df01fa155e997ae04a77
Sarah Vessels <cheshire137@gmail.com>
2022-12-04 15:53:13 -0600

    Add optional player summary to Friend

--- src/models/Friend.ts
+++ src/models/Friend.ts
@@ -1,3 +1,5 @@
+import PlayerSummary from './PlayerSummary'
+
 interface FriendData {
   friend_since: number;
   relationship: string;
@@ -8,11 +10,13 @@ class Friend {
   steamID: string;
   relationship: string;
   friendSince: Date;
+  playerSummary: PlayerSummary | null;
 
   constructor(data: FriendData) {
     this.steamID = data.steamid;
     this.relationship = data.relationship;
     this.friendSince = new Date(data.friend_since * 1000);
+    this.playerSummary = null;
   }
 }
 
