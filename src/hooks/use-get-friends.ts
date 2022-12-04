3027b178ddccf0a8b38194ff221bf23049eabaf6
Sarah Vessels <cheshire137@gmail.com>
2022-12-04 16:34:54 -0600

    Store friends in same key as deleted, require ID

--- src/hooks/use-get-friends.ts
+++ src/hooks/use-get-friends.ts
@@ -9,26 +9,21 @@ interface Results {
   error?: string;
 }
 
-function useGetFriends(steamID?: string | null): Results {
+function useGetFriends(steamID: string): Results {
   const [results, setResults] = useState<Results>({ fetching: true })
 
   useEffect(() => {
     async function fetchFriends() {
-      const cachedFriends = LocalStorage.get('friends')
+      const cachedFriends = LocalStorage.get('steam-friends')
       if (typeof cachedFriends === 'object') {
         setResults({ friends: cachedFriends, fetching: false })
-        return
-      }
-
-      if (!steamID) {
-        setResults({ fetching: false, error: 'Cannot load friends without a Steam ID' })
         return
       }
 
       try {
         const friends = await SteamApi.getFriends(steamID)
         setResults({ friends, fetching: false })
-        LocalStorage.set('friends', friends)
+        LocalStorage.set('steam-friends', friends)
       } catch (err: any) {
         console.error('failed to fetch Steam friends', err)
         setResults({ fetching: false, error: err.message })
