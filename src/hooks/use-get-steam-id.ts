4199bef791b7fc0092416a5fd15da0635d3c2535
Sarah Vessels <cheshire137@gmail.com>
2022-12-04 15:27:12 -0600

    Add useGetSteamID hook

---
+++ src/hooks/use-get-steam-id.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react"
+import SteamApi from '../models/SteamApi'
+import LocalStorage from '../models/LocalStorage'
+
+interface Results {
+  steamID?: string;
+  fetching: boolean;
+  error?: string;
+}
+
+function useGetSteamID(username: string): Results {
+  const [results, setResults] = useState<Results>({ fetching: true })
+
+  useEffect(() => {
+    async function fetchSteamID() {
+      const cachedSteamID = LocalStorage.get('steam-id');
+      if (typeof cachedSteamID === 'string' && cachedSteamID.length > 0) {
+        setResults({ steamID: cachedSteamID, fetching: false })
+        return
+      }
+
+      if (username.length < 1) {
+        setResults({ fetching: false, error: 'Cannot look up Steam ID for blank username' })
+        return
+      }
+
+      try {
+        const steamID = await SteamApi.getSteamID(username)
+        setResults({ steamID, fetching: false })
+        LocalStorage.set('steam-id', steamID)
+      } catch (err: any) {
+        console.error('failed to fetch Steam ID for username ' + username, err)
+        setResults({ fetching: false, error: err.message })
+      }
+    }
+
+    fetchSteamID()
+  }, [username])
+
+  return results
+}
+
+export default useGetSteamID
