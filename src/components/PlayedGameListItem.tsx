8f921a78d76349483f003de7bbe7c59c143f3dcd
Sarah Vessels <cheshire137@gmail.com>
2022-12-10 16:40:23 -0600

    Style games list

---
+++ src/components/PlayedGameListItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Game from '../models/Game'
+import { FormControl, Radio } from '@primer/react'
+
+interface Props {
+  game: Game;
+  loadGame(game: Game): void;
+}
+
+const PlayedGameListItem = ({ game, loadGame }: Props) => {
+  const domId = 'game-' + game.appID
+
+  return <FormControl id={domId} sx={{ my: 1, mr: 3, display: 'flex', alignItems: 'center' }}>
+    <Radio value={game.appID.toString()} onChange={() => loadGame(game)} />
+    <FormControl.Label>{game.name}</FormControl.Label>
+  </FormControl>
+}
+
+export default PlayedGameListItem
--- src/components/PlayedGamesList.tsx
+++ src/components/PlayedGamesList.tsx
@@ -1,30 +1,18 @@
 import React from 'react'
-import SteamGame from './SteamGame'
+import PlayedGameListItem from './PlayedGameListItem'
 import Game from '../models/Game'
+import { Box, RadioGroup } from '@primer/react'
 
 interface Props {
   loadGame(game: Game): void;
   games: Game[];
 }
 
-const PlayedGamesList = ({ loadGame, games }: Props) => {
-  const index = Math.ceil(games.length / 2.0)
-  const column1 = games.slice(0, index)
-  const column2 = games.slice(index)
-
-  return (
-    <section>
-      <h2>Played Games ({games.length})</h2>
-      <div>
-        <ul>
-          {column1.map(game => <SteamGame loadGame={loadGame} game={game} key={game.appID} />)}
-        </ul>
-        <ul>
-          {column2.map(game => <SteamGame loadGame={loadGame} game={game} key={game.appID} />)}
-        </ul>
-      </div>
-    </section>
-  )
-}
+const PlayedGamesList = ({ loadGame, games }: Props) => <RadioGroup name="game">
+  <RadioGroup.Label>Played Games ({games.length})</RadioGroup.Label>
+  <Box display="flex" flexWrap="wrap">
+    {games.map(game => <PlayedGameListItem loadGame={loadGame} game={game} key={game.appID} />)}
+  </Box>
+</RadioGroup>
 
 export default PlayedGamesList;
