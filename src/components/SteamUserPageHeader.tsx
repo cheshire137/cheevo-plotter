3eaa9fd264c5f51dae2adc963b80dd3971287148
Sarah Vessels <cheshire137@gmail.com>
2022-12-10 15:23:13 -0600

    Style user page header

--- src/components/SteamUserPageHeader.tsx
+++ src/components/SteamUserPageHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import PlayerSummary from '../models/PlayerSummary'
+import { Avatar, IconButton, Box, Link, Text } from '@primer/react'
+import { ArrowLeftIcon } from '@primer/octicons-react'
 
 interface Props {
   steamUsername: string;
@@ -16,14 +18,21 @@ const SteamUserPageHeader = ({ playerSummary, steamUsername, onUsernameChange }:
     onUsernameChange('')
   }
 
-  return <h1>
-    <button type="button" onClick={e => clearSteamUsername(e)}>&laquo;</button>
-    Steam <span>/ </span>
-    {playerSummary ? <a href={profileUrl} rel="noreferrer" target="_blank">
-      <img src={playerSummary.avatarmedium} alt={playerSummary.steamid} />
-      <span>{playerSummary.personaname}</span> {playerSummary.realname && <span> {playerSummary.realname}</span>}
-    </a> : <a href={profileUrl} rel="noreferrer" target="_blank">{steamUsername}</a>}
-  </h1>
+  return <Box display="flex" as="h1" alignItems="center">
+    <IconButton
+      variant="invisible"
+      type="button"
+      sx={{ mr: 2 }}
+      icon={ArrowLeftIcon}
+      aria-label="Clear Steam username"
+      onClick={(e: any) => clearSteamUsername(e)} />
+    Steam
+    <Text color="fg.subtle" fontWeight="normal" sx={{ ml: 2 }}> /</Text>
+    {playerSummary ? <Link sx={{ display: 'flex', alignItems: 'center' }} href={profileUrl} rel="noreferrer" target="_blank">
+      <Avatar size={48} sx={{ mx: 2 }} src={playerSummary.avatarmedium} alt={playerSummary.steamid} />
+      <span>{playerSummary.personaname}</span> {playerSummary.realname && <Text fontWeight="normal" sx={{ ml: 2 }}> {playerSummary.realname}</Text>}
+    </Link> : <Link sx={{ ml: 2 }} href={profileUrl} rel="noreferrer" target="_blank">{steamUsername}</Link>}
+  </Box>
 }
 
 export default SteamUserPageHeader
