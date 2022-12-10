24c881c28b7f23f07386e64d8cba99240b83fda7
Sarah Vessels <cheshire137@gmail.com>
2022-12-10 15:22:37 -0600

    Style user error as Flash warning

--- src/components/SteamUserError.tsx
+++ src/components/SteamUserError.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Flash } from '@primer/react'
 
 const SteamUserError = () => {
-  return <div>
+  return <Flash variant="danger">
     <p>Could not find Steam ID for that username.</p>
     <p>Try setting your custom URL in Steam:</p>
     <p><img src={require('./steam-edit-profile.jpg')} width="640" height="321" alt="Edit Steam profile" /></p>
     <p>Then, search here for the name you set in that custom URL.</p>
-  </div>
+  </Flash>
 }
 
 export default SteamUserError
