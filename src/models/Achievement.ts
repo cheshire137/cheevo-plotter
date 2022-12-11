f6e44db537d44b0d3d89ed2eb85db5c1eebabf13
Sarah Vessels <cheshire137@gmail.com>
2022-12-11 14:53:12 -0600

    Don't store players on achievement

--- src/models/Achievement.ts
+++ src/models/Achievement.ts
@@ -1,18 +1,12 @@
-import Player from './Player'
-
 class Achievement {
-  isUnlocked: boolean;
   key: string;
   iconUri: string;
   name: string;
-  players: Player[];
 
-  constructor(isUnlocked: boolean, iconUri: string, name: string, key: string) {
-    this.isUnlocked = isUnlocked;
+  constructor(iconUri: string, name: string, key: string) {
     this.iconUri = iconUri;
     this.name = name;
     this.key = key;
-    this.players = [];
   }
 }
 
