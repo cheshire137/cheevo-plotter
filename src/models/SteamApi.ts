49b4b69477e4e2b6eab024bd7e5f6ce849b35e0d
Sarah Vessels <cheshire137@gmail.com>
2022-12-11 13:47:02 -0600

    Use PlayerSummary compare for sorting

--- src/models/SteamApi.ts
+++ src/models/SteamApi.ts
@@ -12,10 +12,9 @@ enum ResponseType {
 }
 
 const serverUrl = 'http://localhost:8080';
+const maxCacheAgeInSeconds = 86400; // 24 hours
 
 type AchievementsResult = { iconUri?: string, achievements: Achievement[] };
-
-const maxCacheAgeInSeconds = 86400; // 24 hours
 
 class SteamApi {
   // https://wiki.teamfortress.com/wiki/WebAPI/ResolveVanityURL
@@ -32,32 +31,31 @@ class SteamApi {
   }
 
   static async getPlayerSummaries(steamIDs: string[]) {
-    const batches = [];
     // see https://developer.valvesoftware.com/wiki/Steam_Web_API#GetPlayerSummaries_.28v0002.29
-    const batchSize = 100;
-    let index = 0;
+    const batchSize = 100
+    const batches = []
+    let index = 0
+
     while (index < steamIDs.length) {
-      var batch = [];
+      const batch = []
       while (batch.length < batchSize && index < steamIDs.length) {
-        batch.push(steamIDs[index]);
-        index++;
+        batch.push(steamIDs[index])
+        index++
       }
-      batches.push(batch);
+      batches.push(batch)
     }
-    let summaries: any[] = [];
+
+    let summaries: any[] = []
     for (const batch of batches) {
       const result = await this.get('/api/steam?format=json&path=/ISteamUser/GetPlayerSummaries/v0002/' +
-        '&steamids=' + batch.join(','));
+        '&steamids=' + batch.join(','))
       if (result.response) {
-        summaries = summaries.concat(result.response.players || []);
+        summaries = summaries.concat(result.response.players || [])
       }
     }
-    summaries.sort((a, b) => {
-      const aName = a.personaname.toLowerCase();
-      const bName = b.personaname.toLowerCase();
-      return aName.localeCompare(bName);
-    });
-    return summaries.map(ps => new PlayerSummary(ps));
+    const playerSummaries = summaries.map(ps => new PlayerSummary(ps))
+    playerSummaries.sort((a, b) => a.compare(b))
+    return playerSummaries
   }
 
   static async getFriends(steamID: string): Promise<Friend[]> {
@@ -67,12 +65,12 @@ class SteamApi {
     if (data.friendslist && data.friendslist.friends) {
       return data.friendslist.friends.map((d: any) => new Friend(d))
     }
-    throw new Error('Failed to get friends for ' + steamID + '; may not be a public profile.');
+
+    throw new Error('Failed to get friends for ' + steamID + '; may not be a public profile.')
   }
 
   static async getOwnedGames(steamID: string, username?: string): Promise<Game[]> {
-    const data = await this.get('/api/steam?format=json&path=/IPlayerService/GetOwnedGames/v0001/&steamid=' +
-      steamID)
+    const data = await this.get(`/api/steam?format=json&path=/IPlayerService/GetOwnedGames/v0001/&steamid=${steamID}`)
     if (data && data.response && data.response.games) {
       return data.response.games.map((g: any) => new Game({
         appID: g.appid,
@@ -80,6 +78,7 @@ class SteamApi {
         timeLastPlayed: g.rtime_last_played,
       }))
     }
+
     const userDesc = username ? username : `ID ${steamID}`
     throw new Error(`Couldn't load Steam games for ${userDesc}; may not be a public profile.`)
   }
@@ -91,7 +90,8 @@ class SteamApi {
 
   static async getAchievements(steamID: string, appID: number): Promise<AchievementsResult> {
     const xml = await this.get('/api/steam?path=/profiles/' + steamID + '/stats/' + appID + '/achievements/&xml=1',
-      ResponseType.XML);
+      ResponseType.XML)
+
     try {
       const rawResult = await parseStringPromise(xml)
       const achievements: Achievement[] = rawResult.playerstats.achievements[0].achievement.map((a: any) => {
@@ -101,6 +101,7 @@ class SteamApi {
         const achievementKey = a.apiname[0]
         return new Achievement(isUnlocked, achievementIconUri, achievementName, achievementKey)
       })
+
       const gameIconUri = rawResult.playerstats.game[0].gameIcon[0]
       return { iconUri: gameIconUri, achievements }
     } catch (err) {
@@ -111,15 +112,17 @@ class SteamApi {
 
   static async getJsonAchievements(steamID: string, appID: number): Promise<AchievementsResult> {
     const rawResult = await this.get('/api/steam?path=/ISteamUserStats/GetPlayerAchievements/v0001/&appid=' + appID +
-      '&steamid=' + steamID + '&format=json');
+      '&steamid=' + steamID + '&format=json')
     if (typeof rawResult.playerstats.error === 'string') {
-      throw new Error(rawResult.playerstats.error);
+      throw new Error(rawResult.playerstats.error)
     }
-    const schema = await this.getGameSchema(appID);
+
+    const schema = await this.getGameSchema(appID)
     const achievementInfo: { [name: string]: any } = {}
     for (const schemaAchievement of schema.game.availableGameStats.achievements) {
-      achievementInfo[schemaAchievement.name] = schemaAchievement;
+      achievementInfo[schemaAchievement.name] = schemaAchievement
     }
+
     const achievements = rawResult.playerstats.achievements.map((a: any) => {
       const info = achievementInfo[a.apiname]
       const isUnlocked = a.achieved === 1
@@ -127,7 +130,8 @@ class SteamApi {
       const achievementName = info.displayName
       const achievementKey = a.apiname
       return new Achievement(isUnlocked, achievementIconUri, achievementName, achievementKey)
-    });
+    })
+
     // TODO: somehow get game iconUri from JSON API
     return { achievements }
   }
