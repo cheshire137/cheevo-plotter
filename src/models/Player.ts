90580e730c2e5c53cd0f1c6b291020bb3e4fc781
Sarah Vessels <cheshire137@gmail.com>
2022-12-11 14:20:50 -0600

    Add unlockedAchievements to Player

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -1,10 +1,18 @@
+import Achievement from "./Achievement"
+
 class Player {
   steamid: string;
   personaname: string;
+  unlockedAchievements: Achievement[];
 
   constructor(steamid: string, personaname: string) {
     this.steamid = steamid;
     this.personaname = personaname;
+    this.unlockedAchievements = [];
+  }
+
+  addUnlockedAchievement(achievement: Achievement) {
+    this.unlockedAchievements.push(achievement)
   }
 }
 
