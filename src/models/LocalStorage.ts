feb1082a18ee0a4551b67bc9e7843b9187f994ef
Sarah Vessels <cheshire137@gmail.com>
2022-12-11 12:55:05 -0600

    Add LocalStorage hasKey

--- src/models/LocalStorage.ts
+++ src/models/LocalStorage.ts
@@ -3,34 +3,39 @@ const localStorageKey = "cheevo-plotter";
 class LocalStorage {
   static getJSON() {
     if (typeof window === 'undefined') {
-      return {};
+      return {}
     }
     if (!window.localStorage) {
-      console.error('browser does not support local storage');
-      return {};
+      console.error('browser does not support local storage')
+      return {}
     }
-    var appData = window.localStorage.getItem(localStorageKey) || '{}';
-    return JSON.parse(appData);
+    var appData = window.localStorage.getItem(localStorageKey) || '{}'
+    return JSON.parse(appData)
   }
 
   static get(key: string) {
-    var appData = this.getJSON();
-    return appData[key];
+    const appData = this.getJSON()
+    return appData[key]
+  }
+
+  static hasKey(key: string) {
+    const appData = this.getJSON()
+    return key in appData
   }
 
   static set(key: string, value: any) {
-    var appData = this.getJSON();
-    appData[key] = value;
+    var appData = this.getJSON()
+    appData[key] = value
     if (typeof window !== 'undefined') {
-      window.localStorage.setItem(localStorageKey, JSON.stringify(appData));
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
     }
   }
 
   static delete(key: string) {
-    var appData = this.getJSON();
-    delete appData[key];
+    const appData = this.getJSON()
+    delete appData[key]
     if (typeof window !== 'undefined') {
-      window.localStorage.setItem(localStorageKey, JSON.stringify(appData));
+      window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
     }
   }
 }
