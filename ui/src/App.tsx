51ee9eb67093e179db26f75964a2db49d8dadeb9
Sarah Vessels <cheshire137@gmail.com>
2025-12-20 14:24:47 -0600

    List owned games when signed in, rm old PlayedGame* components

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import type {PropsWithChildren} from 'react'
-import {BaseStyles, Button, Link, PageLayout, ThemeProvider, Tooltip, Spinner} from '@primer/react'
+import {BaseStyles, Button, Heading, Link, PageLayout, ThemeProvider, Tooltip, Spinner} from '@primer/react'
 import {useGetCurrentUser} from './queries/use-get-current-user'
 import {useGetGames} from './queries/use-get-games'
 import '@primer/primitives/dist/css/functional/themes/light.css'
@@ -7,13 +7,14 @@ import '@primer/primitives/dist/css/primitives.css'
 import './App.css'
 
 function App() {
-  const {data: currentUser, isPending} = useGetCurrentUser()
-  const {data: ownedGames} = useGetGames({steamId: currentUser?.steamId})
+  const {data: currentUser, isPending: isCurrentUserPending} = useGetCurrentUser()
+  const {data: ownedGames, isPending: isOwnedGamesPending} = useGetGames({steamId: currentUser?.steamId})
 
   return (
     <ProviderStack>
       <PageLayout>
         <PageLayout.Header>
+          <Heading as="h1">Cheevo plotter</Heading>
           {currentUser ? (
             <form method="POST" action={`${import.meta.env.VITE_BACKEND_URL}/user/logout`}>
               <Tooltip text={`Signed in as ${currentUser.name}`}>
@@ -27,8 +28,19 @@ function App() {
           )}
         </PageLayout.Header>
         <PageLayout.Content>
-          {currentUser?.name}
-          {isPending && <Spinner />}
+          {(isCurrentUserPending || isOwnedGamesPending) && <Spinner />}
+          {ownedGames && (
+            <>
+              {currentUser && <Heading as="h2">{currentUser.name}&rsquo;s owned games</Heading>}
+              <ul>
+                {ownedGames.map(game => (
+                  <li key={game.appId}>
+                    {game.name}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
         </PageLayout.Content>
       </PageLayout>
     </ProviderStack>
