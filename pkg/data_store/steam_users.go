995b6e515f64fff0ddf57b3cf2127a63b1dd262f
Sarah Vessels <cheshire137@gmail.com>
2025-12-22 15:07:29 -0600

    Cache friend data

--- pkg/data_store/steam_users.go
+++ pkg/data_store/steam_users.go
@@ -5,6 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+
+	"github.com/cheshire137/cheevo-plotter/pkg/steam"
+	"github.com/cheshire137/cheevo-plotter/pkg/util"
 )
 
 type SteamUser struct {
@@ -15,21 +18,86 @@ type SteamUser struct {
 	FriendIds  []string `json:"friendIds"`
 }
 
+func NewSteamUser(data *steam.SteamPlayerSummary) *SteamUser {
+	return &SteamUser{
+		Id:         data.Id,
+		Name:       data.Name,
+		AvatarUrl:  data.AvatarUrl,
+		ProfileUrl: data.ProfileUrl,
+		FriendIds:  []string{},
+	}
+}
+
 const friendIdSeparator = ","
+
+func (ds *DataStore) GetSteamUsers(ids []string) ([]*SteamUser, error) {
+	users := []*SteamUser{}
+	if len(ids) < 1 {
+		return users, nil
+	}
+
+	placeholders := strings.Repeat("?,", len(ids))
+	placeholders = placeholders[:len(placeholders)-1] // drop trailing comma
+
+	query := `SELECT id, name, avatar_url, profile_url, friend_ids FROM steam_users WHERE id IN (` + placeholders +
+		`) ORDER BY name ASC`
+	stmt, err := ds.db.Prepare(query)
+	if err != nil {
+		return users, fmt.Errorf("failed to prepare query for listing Steam users: %v", err)
+	}
+	defer stmt.Close()
+
+	args := make([]interface{}, len(ids))
+	for i, id := range ids {
+		args[i] = id
+	}
+
+	rows, err := stmt.Query(args...)
+	if err != nil {
+		return users, fmt.Errorf("failed to query Steam users: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		user := &SteamUser{}
+		var name, avatarUrl, profileUrl, friendIds sql.NullString
+		err := rows.Scan(&user.Id, &name, &avatarUrl, &profileUrl, &friendIds)
+		if err != nil {
+			return users, fmt.Errorf("failed to scan Steam user row: %v", err)
+		}
+
+		if name.Valid {
+			user.Name = name.String
+		}
+		if avatarUrl.Valid {
+			user.AvatarUrl = avatarUrl.String
+		}
+		if profileUrl.Valid {
+			user.ProfileUrl = profileUrl.String
+		}
+		if friendIds.Valid {
+			user.FriendIds = strings.Split(friendIds.String, friendIdSeparator)
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
 
 func (ds *DataStore) GetSteamUser(id string) (*SteamUser, error) {
 	user := &SteamUser{}
 	query := `SELECT id, name, avatar_url, profile_url, friend_ids FROM steam_users WHERE id = ?`
 	stmt, err := ds.db.Prepare(query)
 	if err != nil {
-		return nil, fmt.Errorf("failed to prepare query for getting Steam user: %w", err)
+		return nil, fmt.Errorf("failed to prepare query for getting Steam user: %v", err)
 	}
 	defer stmt.Close()
 
 	var name, avatarUrl, profileUrl, friendIds sql.NullString
 	err = stmt.QueryRow(id).Scan(&user.Id, &name, &avatarUrl, &profileUrl, &friendIds)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get Steam user: %w", err)
+		return nil, fmt.Errorf("failed to get Steam user: %v", err)
 	}
 
 	if name.Valid {
@@ -58,19 +126,20 @@ func (ds *DataStore) UpsertSteamUser(user *SteamUser) error {
 		return errors.New("user ID is required")
 	}
 
+	util.LogInfo("Saving Steam user " + user.Name)
 	query := `INSERT INTO steam_users (id, name, avatar_url, profile_url, friend_ids) VALUES (?, ?, ?, ?, ?)
 		ON CONFLICT(id) DO UPDATE SET name=excluded.name, avatar_url=excluded.avatar_url,
 			profile_url=excluded.profile_url, friend_ids=excluded.friend_ids`
 	stmt, err := ds.db.Prepare(query)
 	if err != nil {
-		return fmt.Errorf("failed to prepare query for inserting Steam user %w", err)
+		return fmt.Errorf("failed to prepare query for inserting Steam user %v", err)
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(user.Id, user.Name, user.AvatarUrl, user.ProfileUrl,
 		strings.Join(user.FriendIds, friendIdSeparator))
 	if err != nil {
-		return fmt.Errorf("failed to insert Steam user: %w", err)
+		return fmt.Errorf("failed to insert Steam user: %v", err)
 	}
 
 	return nil
--- pkg/server/get_steam_friends_handler.go
+++ pkg/server/get_steam_friends_handler.go
@@ -6,12 +6,20 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/cheshire137/cheevo-plotter/pkg/data_store"
 	"github.com/cheshire137/cheevo-plotter/pkg/steam"
 	"github.com/cheshire137/cheevo-plotter/pkg/util"
 )
 
+type SteamFriend struct {
+	Id         string `json:"steamId"`
+	Name       string `json:"name"`
+	AvatarUrl  string `json:"avatarUrl"`
+	ProfileUrl string `json:"profileUrl"`
+}
+
 type SteamFriendsResponse struct {
-	Friends []*steam.SteamPlayerSummary `json:"friends"`
+	Friends []*SteamFriend `json:"friends"`
 }
 
 func (e *Env) GetSteamFriendsHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,13 +38,47 @@ func (e *Env) GetSteamFriendsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	friends := []*steam.SteamPlayerSummary{}
-	if len(user.FriendIds) > 0 {
+	friends := []*SteamFriend{}
+	cachedFriends, err := e.ds.GetSteamUsers(user.FriendIds)
+	if err != nil {
+		ErrorJson(w, err)
+		return
+	}
+
+	existingFriendIds := map[string]bool{}
+	for _, cachedFriend := range cachedFriends {
+		existingFriendIds[cachedFriend.Id] = true
+		friend := &SteamFriend{
+			Id:         cachedFriend.Id,
+			Name:       cachedFriend.Name,
+			AvatarUrl:  cachedFriend.AvatarUrl,
+			ProfileUrl: cachedFriend.ProfileUrl,
+		}
+		friends = append(friends, friend)
+	}
+
+	if len(user.FriendIds) != len(cachedFriends) {
 		client := steam.NewClient(e.config.SteamApiKey)
-		friends, err = client.GetPlayerSummaries(user.FriendIds)
+
+		missingFriendIds := []string{}
+		for _, friendId := range user.FriendIds {
+			if !existingFriendIds[friendId] {
+				missingFriendIds = append(missingFriendIds, friendId)
+			}
+		}
+
+		friends, err := client.GetPlayerSummaries(missingFriendIds)
 		if err != nil {
 			ErrorJson(w, err)
 			return
+		}
+
+		for _, friend := range friends {
+			err = e.ds.UpsertSteamUser(data_store.NewSteamUser(friend))
+			if err != nil {
+				ErrorJson(w, err)
+				return
+			}
 		}
 	}
 
