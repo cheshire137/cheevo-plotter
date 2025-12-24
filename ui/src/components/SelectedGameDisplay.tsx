6718fc507c99f23a1c4d4b03f7dbc3a3d6d0b337
Sarah Vessels <cheshire137@gmail.com>
2025-12-24 15:52:16 -0600

    Don't show blankslate for game when not signed in

--- ui/src/components/SelectedGameDisplay.tsx
+++ ui/src/components/SelectedGameDisplay.tsx
@@ -4,10 +4,13 @@ import {Heading} from '@primer/react'
 import {SelectedFriendsList} from './SelectedFriendsList'
 import {useSelectedGame} from '../contexts/selected-game-context'
 import {AchievementsList} from './AchievementsList'
+import {useGetCurrentUser} from '../queries/use-get-current-user'
 
 export function SelectedGameDisplay() {
   const {selectedGame} = useSelectedGame()
+  const {data: currentUser} = useGetCurrentUser()
 
+  if (!currentUser) return null
   if (!selectedGame) {
     return (
       <Blankslate>
